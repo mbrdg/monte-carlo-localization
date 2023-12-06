10cc2206b63d2b8a6e6518563475a30734db653e
Miguel Rodrigues <migb.rodrigues@gmail.com>
2023-12-06 16:27:15 +0100

    Simplified compute_sensor_points()

--- src/robot.py
+++ src/robot.py
@@ -1,4 +1,5 @@
 import math
+import itertools
 import random
 
 import numpy as np
@@ -57,59 +58,46 @@ class Robot:
     def apply_move(self, position):
         self.position = position
 
-    def compute_sensor_points(self):
-        x, y = self.position
-        start_angle = self.angle - self.aperture / 2.0
-        stop_angle = self.angle + self.aperture / 2.0
-
-        return (
-            (x + math.cos(angle) * self.max_sensor_range,
-             y + math.sin(angle) * self.max_sensor_range)
-            for angle in np.linspace(start_angle, stop_angle, num=self.num_sensors)
-        )
-
-    @staticmethod
-    def line_line_intersection(line1, line2):
-
-        line1 = LineString(line1)
-        line2 = LineString(line2)
-
-        res = line1.intersection(line2)
-
-        if res.geom_type == 'Point':
-            return res.x, res.y
-
-        return None
-
-    def measure(self, walls):
-        measurements = []
-        for sample in self.compute_sensor_points():
-
-            intersections = (Robot.line_line_intersection(
-                (wall.pos1*GRID_SIZE, wall.pos2*GRID_SIZE), (self.position, sample)) for wall in walls)
-
-            distances = (math.dist(self.position, point) if point is not None else self.max_sensor_range
-                         for point in intersections)
-
-            measure = min(distances)
-
-            if measure > self.max_sensor_range:
-                measure = self.max_sensor_range
-            noise = random.gauss(0.0, SIGMA_MEASURE)
-            measurements.append(measure + noise)
-
-        return measurements
-
-    def transform_measures_into_points(self, distances):
+    def compute_sensor_points(self, distances=None):
         x, y = self.position
         start_angle = self.angle - self.aperture / 2.0
         stop_angle = self.angle + self.aperture / 2.0
         angles = np.linspace(start_angle, stop_angle, num=self.num_sensors)
 
-        return tuple(
+        if distances is None:
+            distances = itertools.repeat(self.max_sensor_range, self.num_sensors)
+
+        return (
             (x + math.cos(angle) * distance, y + math.sin(angle) * distance)
             for angle, distance in zip(angles, distances)
         )
+
+    @staticmethod
+    def intersects(line1, line2):
+        line1 = LineString(line1)
+        line2 = LineString(line2)
+
+        res = line1.intersection(line2)
+        return (res.x, res.y) if res.geom_type == "Point" else None
+
+    def measure(self, walls):
+        measurements = []
+        for sample in self.compute_sensor_points():
+            intersections = (
+                Robot.intersects(
+                    (wall.pos1 * GRID_SIZE, wall.pos2 * GRID_SIZE), (self.position, sample)
+                ) for wall in walls
+            )
+            distances = (
+                math.dist(self.position, point)
+                for point in intersections if point is not None
+            )
+
+            measure = min(min(distances, default=self.max_sensor_range), self.max_sensor_range)
+            noise = random.gauss(0.0, SIGMA_MEASURE)
+            measurements.append(measure + noise)
+
+        return measurements
 
     def draw(self, screen, walls):
         x, y = self.position
@@ -123,7 +111,6 @@ class Robot:
             pygame.draw.line(screen, (0, 100, 100), (x, y), sensor_point, 3)
 
         distances = self.measure(walls)
-        points = self.transform_measures_into_points(distances)
-        # points = tuple(self.compute_sensor_points())
+        points = tuple(self.compute_sensor_points(distances))
         for point in points:
             pygame.draw.line(screen, (255, 0, 0), (x, y), point, 3)
